004d6124ac015a563953ef1dc1f81c011d46cadc
Nick Maludy <nmaludy@gmail.com>
2020-09-03 21:05:05 -0400

    Update from voxpupuli modulesync_config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,18 @@
-RSpec.configure do |c|
-  c.mock_with :rspec
-  c.after(:suite) do
-    RSpec::Puppet::Coverage.report!
+# This file is managed via modulesync
+# https://github.com/voxpupuli/modulesync
+# https://github.com/voxpupuli/modulesync_config
+
+# puppetlabs_spec_helper will set up coverage if the env variable is set.
+# We want to do this if lib exists and it hasn't been explicitly set.
+ENV['COVERAGE'] ||= 'yes' if Dir.exist?(File.expand_path('../../lib', __FILE__))
+
+require 'voxpupuli/test/spec_helper'
+
+if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
+  facts = YAML.safe_load(File.read(File.join(__dir__, 'default_module_facts.yml')))
+  if facts
+    facts.each do |name, value|
+      add_custom_fact name.to_sym, value
+    end
   end
 end
-require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rspec-puppet-facts'
-include RspecPuppetFacts
